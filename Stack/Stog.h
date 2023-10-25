8788a751b71a9a1ad5f7c5d7c7bf0d47d514367a
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2023-10-25 15:56:43 +0200

    Add files via upload

---
+++ Stack/Stog.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+//tip podataka elemenata stoga
+typedef int tip_elementaT;
+
+//element stoga
+struct cvor {
+	tip_elementaT podatak; // podatak na stogu
+	struct cvor* sljedeci; // pokazivac na perthodni cvor
+};
+
+class Stog
+{
+	int koliko; // broj elemenata na stogu
+public:
+	cvor* vrh; // pokazivac na vrh stoga
+	//osnovne operacije
+	int velicina() const; // vraca velicinu stoga
+	bool prazan() const; // vraca true ako je stog prazan
+	void push(tip_elementaT _podatak); // stavaljanje na stog
+	tip_elementaT pop(); // uzimanje sa stoga
+	//izvedene funkicje
+	void ocisti(); //cisti stog
+	//konstruktor i destrukotr
+	Stog(void);
+	~Stog(void);
+};
