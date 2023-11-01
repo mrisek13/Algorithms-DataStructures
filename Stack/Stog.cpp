778e58bd92e2319c7b2989421328f9268112f7c4
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2023-11-01 12:09:29 +0100

    Update Stog.cpp

--- Stack/Stog.cpp
+++ Stack/Stog.cpp
@@ -2,57 +2,57 @@
 
 //konstruktor
 Stog::Stog(void) {
-    koliko = 0;
-    vrh = NULL;
+	koliko = 0;
+	vrh = NULL;
 }
 
 //destruktor
 Stog::~Stog(void) {
-    ocisti();
+	ocisti();
 }
 
 //vraca velicinu stoga
 int Stog::velicina() const
 {
-    return koliko;
+	return koliko;
 }
 
 //vraca true ako je stog prazan
 bool Stog::prazan() const
 {
-    return koliko == 0;
+	return koliko == 0;
 }
 
 //stavljanje na stog
 void Stog::push(tip_elementaT _podatak)
 {
-    cvor* novi = new cvor; // kreiramo novi cvor
-    novi->podatak = _podatak; // dodajemo podatak u cvor
-    novi->sljedeci = vrh; // novi gleda na prethodni
-    vrh = novi; // novi cvor postaje novi vrh stoga
-    koliko++; // povecamo brojac elemenata na stogu
+	Cvor* novi = new Cvor; // kreiramo novi cvor
+	novi->podatak = _podatak; // dodajemo podatak u cvor
+	novi->sljedeci = vrh; // novi gleda na prethodni
+	vrh = novi; // novi cvor postaje novi vrh stoga
+	koliko++; // povecamo brojac elemenata na stogu
 }
 
 tip_elementaT Stog::pop()
 {
-    if (!prazan()) {
-        tip_elementaT _podatak = vrh->podatak; // zapamtimo podatak
-        cvor* c = vrh->sljedeci; // zapamtimo novi vrh
-        delete vrh; //obrisemo stari vrh
-        vrh = c; //postavljamo novi vrh
-        koliko--; // smanjimo brojac elemenata na stogu
-        return _podatak; // vratimo podatak sa stoga
-    }
-    else {
-        cout << "GRAŠKA: Stog je prazan!" << endl;
-        return NULL;
-    }
+	if (!prazan()) {
+		tip_elementaT _podatak = vrh->podatak; // zapamtimo podatak
+		Cvor* c = vrh->sljedeci; // zapamtimo novi vrh
+		delete vrh; //obrisemo stari vrh
+		vrh = c; //postavljamo novi vrh
+		koliko--; // smanjimo brojac elemenata na stogu
+		return _podatak; // vratimo podatak sa stoga
+	}
+	else {
+		cout << "GREÅ KA: Stog je prazan!" << endl;
+		return NULL;
+	}
 }
 
 //cisti stog
 void Stog::ocisti()
 {
-    while (vrh != NULL)
-        tip_elementaT _podatak = pop(); //uzimamo sa stoga dok ima
+	while (vrh != NULL)
+		tip_elementaT _podatak = pop(); //uzimamo sa stoga dok ima
 }
 
